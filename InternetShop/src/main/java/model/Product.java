2083a415522d51d8d640275ae72702cafc6a6ea9
Vlad Verbytskyi <vlad.verbytskyi2019@gmail.com>
2020-01-08 22:20:23 +0200

    Added User and Product

--- InternetShop/src/main/java/model/Product.java
+++ InternetShop/src/main/java/model/Product.java
@@ -1,4 +1,90 @@
 package model;
 
 public class Product {
+    private String name;
+    private String brandName;
+    private String type;
+    private Integer productId;
+    private String description;
+    private Integer price;
+    private boolean inStock;
+
+    public Product(String name, String brandName, String type, Integer productId,
+                   String description, Integer price, boolean inStock) {
+        this.name = name;
+        this.brandName = brandName;
+        this.type = type;
+        this.productId = productId;
+        this.description = description;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    public Product() {
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Product(String name, String brandName) {
+        this.name = name;
+        this.brandName = brandName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
 }
--- InternetShop/src/main/java/model/User.java
+++ InternetShop/src/main/java/model/User.java
@@ -1,7 +1,85 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String name;
     private Integer id;
     private String login;
+    private String password;
+    private String email;
+    private Bucket bucket;
+    private List<Order> orders = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String name, Integer id, String login, String password, String email) {
+        this.name = name;
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.email = email;
+        this.bucket = new Bucket();
+        this.orders = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Bucket getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(Bucket bucket) {
+        this.bucket = bucket;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
+
+
