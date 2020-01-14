7dfd4aa5d7d367e959e72b8412166ab9591a19dc
Ronashovkova <rona_t@icloud.com>
2020-01-14 20:28:04 +0200

    dao: added UserDao and UserDaoImpl

--- InternetShop/src/main/java/dao/UserDao.java
+++ InternetShop/src/main/java/dao/UserDao.java
@@ -2,11 +2,15 @@ package dao;
 
 import model.User;
 
+import java.util.Map;
+
 /**
  * Created by Igor on 10/8/2019.
  */
 public interface UserDao {
-    void setUserIntoDatabase(User user);
-    User getUserFromDatabase();
-    void checkUserInDatabase(User user);
+    void setUserIntoDatabase(User user); //create
+    User getUserByName(String name); //read
+    Map<String, User> getAll();
+    void checkUserInDatabase(User user); //update
+    void deleteUserFromDatabase(User user); //delete
 }
--- InternetShop/src/main/java/dao/UserDaoImpl.java
+++ InternetShop/src/main/java/dao/UserDaoImpl.java
@@ -6,18 +6,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserDaoImpl implements UserDao {
-    private Map<String, User> usersList = new HashMap<>();
+
+    private Map<String, User> users = new HashMap<>();
 
     @Override
     public void setUserIntoDatabase(User user) {
+        users.put(user.getName(), user);
+        System.out.println("User " + user.getName() + " added to the database");
     }
 
     @Override
-    public User getUserFromDatabase() {
-        return null;
+    public User getUserByName(String name) {
+        return users.get(name);
+    }
+
+    @Override
+    public Map<String, User> getAll() {
+        return users;
     }
 
     @Override
     public void checkUserInDatabase(User user) {
+        if (users.containsKey(user.getName())) {
+            System.out.println("User " + user.getName() + " exists.");
+        } else {
+            System.out.println("User " + user.getName() + "doesn't exist.");
+        }
+    }
+
+    @Override
+    public void deleteUserFromDatabase(User user) {
+        users.remove(user.getName(), user);
+        System.out.println("User " + user.getName() + " deleted from database");
     }
 }
