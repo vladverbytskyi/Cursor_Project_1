66c57a53099420aee518c195327865574b78afa4
MarDatsko <datskomar@gmail.com>
2020-01-20 17:23:32 +0200

    implemented Login method, and Registration method

---
+++ InternetShop/src/main/java/service/RegisterService.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface RegisterService {
+    boolean registration (String name, String login, String password, String email);
+}
--- InternetShop/src/main/java/service/UserServiceImpl.java
+++ InternetShop/src/main/java/service/UserServiceImpl.java
@@ -1,8 +1,18 @@
 package service;
 
-public class UserServiceImpl implements service.UserService {
+import dao.UserDao;
+import dao.UserDaoImpl;
+import model.User;
+
+public class UserServiceImpl implements UserService {
     @Override
-    public boolean login(String username, String password) {
-        return false;
+    public boolean login(String login, String password) {
+        boolean isCreateUser = false;
+        UserDao userDao = new UserDaoImpl();
+        User userWithDatabase = userDao.getUserByLogin(login);
+        if (userWithDatabase.getLogin().equals(login) && userWithDatabase.getPassword().equals(password)){
+            isCreateUser = true;
+        }
+        return isCreateUser;
     }
 }
